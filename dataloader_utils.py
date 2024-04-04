19eee7956d1cd2a2780b89f0a68cedae2b0e8836
jwl25b <44102199+jwl25b@users.noreply.github.com>
2024-04-04 00:11:50 -0400

    add support for permutedAndSplitttedMNIST

--- dataloader_utils.py
+++ dataloader_utils.py
@@ -6,6 +6,7 @@ import torchvision
 
 from permutedMNIST import PermutedMNIST
 from splittedMNIST import SplittedMNIST
+from permutedAndSplittedMNIST import PermutedAndSplittedMNIST
 
 def get_conbined_permute_mnist(num_task, batch_size, random_seed):
     assert num_task>0
@@ -63,6 +64,47 @@ def get_conbined_split_mnist(num_task, batch_size, random_seed):
         class_indices = classes[j]
         train_datasets[j] = SplittedMNIST(train=True, class_indices=class_indices, task_num=j)
         test_datasets[j] = SplittedMNIST(train=False, class_indices=class_indices, task_num=j)
+
+    train_dataset = data.ConcatDataset([x for _,x in train_datasets.items()])
+    test_dataset = data.ConcatDataset([x for _,x in test_datasets.items()])
+
+    train_dataloader = data.DataLoader(train_dataset,
+                                                  batch_size=batch_size,
+                                                  shuffle = False,
+                                                  num_workers = 4)
+    test_dataloader = data.DataLoader(test_dataset,
+                                                  batch_size=batch_size,
+                                                  shuffle = False)
+
+
+    return random.sample(list(train_dataloader), len(train_dataloader)), random.sample(list(test_dataloader), len(test_dataloader))
+
+def get_conbined_permute_and_split_mnist(num_task, batch_size, random_seed):
+    assert num_task<=5 and num_task>0
+    np_rand.seed(random_seed)
+    classes = {}
+    i=0
+    while(i<num_task):
+        current_class = np_rand.choice(range(10), 2, replace=False)
+        if check_instance(current_class, classes.items()):
+            continue
+        else:
+            classes[i]=(current_class)
+            i+=1
+
+    classes = [list(x) for _, x in classes.items()]
+    print(f"split classes: {classes}")
+
+    train_datasets = {}
+    test_datasets = {}
+
+    idx = list(range(28 * 28))
+    np_rand.shuffle(idx)
+    
+    for j in range(num_task):
+        class_indices = classes[j]
+        train_datasets[j] = PermutedAndSplittedMNIST(train=True, permute_idx=idx, class_indices=class_indices, task_num=j)
+        test_datasets[j] = PermutedAndSplittedMNIST(train=False, permute_idx=idx, class_indices=class_indices, task_num=j)
 
     train_dataset = data.ConcatDataset([x for _,x in train_datasets.items()])
     test_dataset = data.ConcatDataset([x for _,x in test_datasets.items()])
