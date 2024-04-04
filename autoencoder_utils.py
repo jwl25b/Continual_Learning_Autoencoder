4e5b2d188bfedf8bf7687d3a63746476d462e81e
jwl25b <44102199+jwl25b@users.noreply.github.com>
2024-04-04 00:13:00 -0400

    update find_num_of_outliers method

--- autoencoder_utils.py
+++ autoencoder_utils.py
@@ -10,9 +10,10 @@ def find_best_autoencoders(batchwise_data, autoencoders):
         best_index=-1
         for index, autoencoder in autoencoders.items():
             prediction = autoencoder.get_prediction(batchwise_data)
-            reconstruction_error_data = autoencoder.get_reduced_loss(prediction, batchwise_data)
             reconstruction_error_autoencoder = autoencoder.mean
+            reconstruction_error_data = autoencoder.get_reduced_loss(prediction, batchwise_data)
             relatedness = find_relatedness(reconstruction_error_autoencoder, reconstruction_error_data)
+            #print(f"reconError for index {index}: {reconstruction_error_autoencoder}, {reconstruction_error_data}, {relatedness}")
             if relatedness.item()<minimum_relatedness:
                 minimum_relatedness=relatedness
                 best_index = index
@@ -23,5 +24,7 @@ def find_num_of_outliers(batchwise_data, model):
         model_outputs = model.get_prediction(batchwise_data)
         unreduced_loss = model.get_unreduced_loss(model_outputs, batchwise_data)
         element_mean = torch.mean(unreduced_loss, axis=1)
-        outliers = batchwise_data.shape[0] - sum([item>model.mean - 3 * model.std and item<model.mean + 3 * model.std for item in element_mean]).item()
+        #outliers = batchwise_data.shape[0] - sum([item>model.mean - 3 * model.std and item<model.mean + 3 * model.std for item in element_mean]).item()
+        #print(element_mean,  model.mean + 3 * model.std)
+        outliers = sum([item >= model.mean + 3 * model.std for item in element_mean]).item()
         return outliers
